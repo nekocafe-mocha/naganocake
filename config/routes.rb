1ce9734787e0ecd40bd1a5aa89073391d1c9b306
kohayan <disney.2381509@gmail.com>
2020-04-16 12:03:11 +0000

    add　AdminRouting to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   devise_for :customers
+
+  namespace :admin do
+  	resources :customers, only: [:index, :show, :edit, :update]
+  	resources :items, except: [:destroy]
+  	resources :categories, only: [:index, :create, :edit, :update]
+  	resources :orders, only: [:index, :show, :update] do
+  		resources :order_items, only: [:update]
+  	end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
