051f0a08b5499c5a2cf4182210486d017c22bb66
natsumiko000 <k.natsu5454@gmail.com>
2020-04-16 14:09:39 +0000

    [fix]routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 	root 'home#top'
   	devise_for :customers
-  	resources　:customers, only: [:show, :edit, :update]
-  	resources　:order_items, only: [:create]
-  	resources　:deliveries, [:index, :create, :edit, :update, :destroy]
+  	resources :customers, only: [:show, :edit, :update]
+  	resources :order_items, only: [:create]
+  	resources :deliveries, only: [:index, :create, :edit, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
