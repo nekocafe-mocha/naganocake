659a6374e71948c42a4b6d7827e39aa6eca096f7
kohayan <disney.2381509@gmail.com>
2020-04-16 03:18:32 +0000

    fix conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_15_120935) do
+
+ActiveRecord::Schema.define(version: 2020_04_15_081501) do
+
+  create_table "order_items", force: :cascade do |t|
+    t.integer "order_id", null: false
+    t.integer "item_id", null: false
+    t.integer "price", null: false
+    t.integer "quantity", null: false
+    t.integer "status", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.string "name", null: false
+    t.string "postal_code", null: false
+    t.string "address", null: false
+    t.string "phone", null: false
+    t.integer "postage", default: 800, null: false
+    t.integer "total_price", null: false
+    t.integer "pay_select", default: 0, null: false
+    t.integer "status", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 
   create_table "categories", force: :cascade do |t|
     t.string "name", null: false
