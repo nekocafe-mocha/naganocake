b760902a74a5e76112aec2ad604123c4e150107b
suisui0903 <suisuisui0903@gmail.com>
2020-04-19 09:38:14 +0000

    deliveries view

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,7 +1,7 @@
 class Delivery < ApplicationRecord
   validates :customer_id, :name, :address, presence: true
   validates :postal_code, presence: true, format: {with: /\A\d{7}\z/}
-  validates :phone, presence: true, format: { with:/\A\d{10}$|^\d{11}\z/}
+
 
   belongs_to :customer
 end
