355ab6c0e1e43eed8c371c0cbb0fc0d326f40913
kohayan <disney.2381509@gmail.com>
2020-04-20 06:33:44 +0000

    create views/cart_item

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,14 +1,35 @@
 class CartItemsController < ApplicationController
-	def create
+
+    def index
+    	@cart_items = current_customer.cart_items
     end
 
-    def destroy
+    def create
     end
 
     def update
+    	cart_item = CartItem.find(params[:id])
+    	cart_item.update(cart_item_params)
+    	redirect_to cart_items_path
     end
 
-    def index
+    def destroy
+    	cart_item = CartItem.find(params[:id])
+    	cart_item.destroy
+    	redirect_to cart_items_path
     end
+
+    def cart_destroy
+    	cart_items = current_customer.cart_items
+    	cart_items.destroy_all
+    	redirect_to cart_items_path
+    end
+
+    private
+
+    def cart_item_params
+    	params.require(:cart_item).permit(:quantity)
+    end
+
 
 end
