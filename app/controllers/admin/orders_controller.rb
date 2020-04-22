64f406b587f7733a9a05484b426859054ef8c590
kohayan <disney.2381509@gmail.com>
2020-04-22 13:47:34 +0000

    fix admin/order

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,11 +1,23 @@
 class Admin::OrdersController < ApplicationController
 
   def index
+  	@orders = Order.all
   end
 
   def show
+  	@order = Order.find(params[:id])
+  	@order_items = OrderItem.where(order_id: params[:id])
   end
 
   def update
+  	order = Order.find(params[:id])
+  	order.update(order_params)
+  	redirect_to admin_order_path(order)
+  end
+
+  private
+
+  def order_params
+  	params.require(:order).permit(:status)
   end
 end
