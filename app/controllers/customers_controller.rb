30463e1ee2cd7ade3046ee0401830dd27c66ce72
natsumiko000 <k.natsu5454@gmail.com>
2020-04-24 11:00:37 +0000

    [fix]indent

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -5,28 +5,29 @@ class CustomersController < ApplicationController
 
 	def edit
 		customer = Customer.find_by(id: params[:id])
-     	if customer == current_customer
-    		@customer = current_customer
-    	else
-        	redirect_to customer_path(current_customer) and return
-      	end
+		if customer == current_customer
+			@customer = current_customer
+		else
+			redirect_to customer_path(current_customer) and return
+		end
 	end
 
 	def update
 		customer = current_customer
-	    if customer.update(customer_params)
-	      redirect_to customer_path(customer)
-	    else
-	      @customer = customer
-	      render :edit
-	    end
+		if customer.update(customer_params)
+			redirect_to customer_path(customer)
+		else
+			@customer = customer
+			render :edit
+		end
 	end
 
 	def confirm
 	end
- 
+
 	private
-  	def customer_params
-  		params.require(:customer).permit(:family_name, :first_name, :family_name_kana, :first_name_kana, :postal_code, :address, :phone, :email)
-  	end
+
+	def customer_params
+		params.require(:customer).permit(:family_name, :first_name, :family_name_kana, :first_name_kana, :postal_code, :address, :phone, :email)
+	end
 end
