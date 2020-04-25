9b9d98c9f3adbd3ee2dccf76549c32f3922b3d9a
kohayan <disney.2381509@gmail.com>
2020-04-25 06:35:01 +0000

    fix IROIRO

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,20 @@
 class ApplicationController < ActionController::Base
 
 	def after_sign_in_path_for(resource)
-	  case resource
-	   when Customer
-	     root_path
-	   when Admin
-	     admin_home_top_path
-	  end
+	  	case resource
+	   		when Customer
+	     		root_path
+	   		when Admin
+	     		admin_home_top_path
+	  	end
+	end
+
+	def after_sign_out_path_for(resource)
+		case resource
+		   	when :customer
+		     	new_customer_session_path
+		   	when :admin
+		     	new_admin_session_path
+		end
 	end
 end
